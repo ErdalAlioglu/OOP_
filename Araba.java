a49efcfdb4521be841f661f47115ed87d929d579
erdalalioglu <erdalalioglu@gmail.com>
2022-04-11 17:58:26 +0200

    OOP_

--- Araba.java
+++ Araba.java
@@ -1,6 +1,11 @@
 package day30;
 
 public class Araba {
+    private  String renk;
+    private  String marka;
+    private  String model;
+    private  int modelYili;
+    private double silindirHacmi;
 
 }
 
